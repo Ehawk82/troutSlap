7aec297ccb5d5808434c3846ddfba7f24d9c0429
Jason Graziano <mrehawk82@gmail.com>
2019-01-11 11:53:47 -0500

    second attemp

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,22 @@
+
 var myUI;
 
 myUI = {
     loadOut: () => {
-        const express = require('express');
-        const app = new express();
+	    const { spawn } = require('child_process');
+	    const bat = spawn('cmd.exe', ['/c', '/projects/troutSlap/src/trout.bat']);
 
-        app.get('/', function(request, response){
-             response.sendfile('./src/html/index.html');
-        });
+	    bat.stdout.on('data', (data) => {
+	        console.log(data.toString());
+	    });
+
+	    bat.stderr.on('data', (data) => {
+	        console.log(data.toString());
+	    });
+
+	    bat.on('exit', (code) => {
+	       console.log(`Child exited with code ${code}`);
+	    });
     }
 };
 var a, b, ab;
@@ -20,6 +29,7 @@ ab = +a + +b;
 console.log("Drinking age in NY is " + ab);
 setTimeout(() => { console.log("..."); }, 400);
 setTimeout(() => { console.log("Which is okay"); }, 800);
-setTimeout(() => { console.log("...anyway, let's launch this program shall we?"); }, 1600);
+setTimeout(() => { console.log("*SLAPS USER WITH TROUT*"); }, 1600);
 
 setTimeout(() => { myUI.loadOut(); }, 3200);
+
