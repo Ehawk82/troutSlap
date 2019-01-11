4ad553077dc47c686392f5527d67d33cf26e08ae
Jason Graziano <mrehawk82@gmail.com>
2019-01-10 20:30:45 -0500

    needs help

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
+var myUI;
+
+myUI = {
+    loadOut: () => {
+        const express = require('express');
+        const app = new express();
+
+        app.get('/', function(request, response){
+             response.sendfile('./src/html/index.html');
+        });
+    }
+};
 var a, b, ab;
 
 a = "10";
@@ -5,4 +17,9 @@ b = 11;
 
 ab = +a + +b;
 
-console.log("run app " + ab);
+console.log("Drinking age in NY is " + ab);
+setTimeout(() => { console.log("..."); }, 400);
+setTimeout(() => { console.log("Which is okay"); }, 800);
+setTimeout(() => { console.log("...anyway, let's launch this program shall we?"); }, 1600);
+
+setTimeout(() => { myUI.loadOut(); }, 3200);
