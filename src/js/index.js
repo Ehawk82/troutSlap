4a0ecbb0ef7d669dc6a657bb338f9fe42606c9f3
Jason Graziano <mrehawk82@gmail.com>
2019-01-24 10:27:50 -0500

    mistakes were made

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,11 @@
-
-var myUI;
-
-myUI = {
-    loadOut: () => {
-	    const { spawn } = require('child_process');
-	    const bat = spawn('cmd.exe', ['/c', '/projects/troutSlap/src/trout.bat']);
-
-	    bat.stdout.on('data', (data) => {
-	        console.log(data.toString());
-	    });
-
-	    bat.stderr.on('data', (data) => {
-	        console.log(data.toString());
-	    });
-
-	    bat.on('exit', (code) => {
-	       console.log(`Child exited with code ${code}`);
-	    });
-    }
-};
-var a, b, ab;
-
-a = "10";
-b = 11;
-
-ab = +a + +b;
-
-console.log("Drinking age in NY is " + ab);
-setTimeout(() => { console.log("..."); }, 400);
-setTimeout(() => { console.log("Which is okay"); }, 800);
-setTimeout(() => { console.log("*SLAPS USER WITH TROUT*"); }, 1600);
-
-setTimeout(() => { myUI.loadOut(); }, 3200);
+var express=require('express');
+var app=express();
+app.set('view engine','jade');
+app.get('/',function(req,res)
+{
+});
+var server=app.listen(3000,function()
+{
+});
+console.log(server);
 
