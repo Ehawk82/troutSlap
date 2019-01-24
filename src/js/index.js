a166cd3b7520879be6ee812e1f2fe2b2c12c7676
Jason Graziano <mrehawk82@gmail.com>
2019-01-24 13:37:26 -0500

    changes man, changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,3 @@
-var express=require('express');
-var app=express();
-app.set('view engine','jade');
-app.get('/',function(req,res)
-{
-});
-var server=app.listen(3000,function()
-{
-});
-console.log(server);
+console.log("gonna do things differently");
+
 
