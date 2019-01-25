b1aa82cd36d14bff3c3e80b4ca56a7e4fd82eddc
Jason Graziano <mrehawk82@gmail.com>
2019-01-25 12:58:08 -0500

    made it recycle

--- src/js/main.js
+++ src/js/main.js
@@ -19,12 +19,21 @@ myUI = {
             var whoosh = new Audio("src/assets/whoosh.wav"),
                 tic = new Audio("src/assets/tic.wav"),
                 slap = new Audio("src/assets/slap.wav");
+
             troutBtn.onclick = null;
+
             playClick(tic);
+
             makeLock(troutBtn);
+
             var troutHolder = createEle("div"),
                 troutImg = createEle("div"),
-                slapImg = createEle("div");
+                slapImg = createEle("div"),
+                ooofBtn = createEle("button");
+            
+            ooofBtn.innerHTML = "\"Ooof!\"";
+            ooofBtn.className = "ooofBtn";
+            ooofBtn.onclick = myUI.resetWidget(troutBtn, troutHolder, troutImg, slapImg, ooofBtn);
 
             slapImg.innerHTML = "&nbsp;";
             slapImg.className = "slapImg";
@@ -35,11 +44,10 @@ myUI = {
             troutHolder.innerHTML = "&nbsp;";
             troutHolder.className = "troutHolder";
             troutHolder.append(slapImg);
-            troutHolder.append(troutImg);        
+            troutHolder.append(troutImg);       
+            troutHolder.append(ooofBtn);       
 
- 
             body.append(troutHolder);
-            
             
             setTimeout(() => {
             	makeFull(troutHolder);
@@ -47,17 +55,36 @@ myUI = {
                     playClick(whoosh);
             		makeFull(troutImg);
             		setTimeout(() => {
-                        
-
             		    makeFull(slapImg);
                         setTimeout(() => {
                             playClick(slap);
+                            makeFull(ooofBtn);
                         }, 800)
             	    }, 400);
             	}, 400);
             }, 400);
 		}
-	}
+	},
+    resetWidget: (troutBtn, troutHolder, troutImg, slapImg, ooofBtn) => {
+        return () => {
+            deleteThis(ooofBtn);
+            setTimeout(() => {
+                takeFull(slapImg);
+                setTimeout(() => {
+                    takeFull(troutImg);
+                    setTimeout(() => {
+                        takeFull(troutHolder);
+                        setTimeout(() => {
+                            deleteThis(troutHolder);
+                        }, 800)
+                    }, 600);
+                }, 400);
+            }, 200);
+
+            takeLock(troutBtn);
+            troutBtn.onclick = myUI.generateTrout(troutBtn);
+        };
+    }
 };
 
 window.onload = () => {
